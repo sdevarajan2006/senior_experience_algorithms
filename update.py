9edbe28c2033a1d1862646711ad4c99c9e399c3e
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2023-10-04 08:14:04 -0500

    Create update.py

---
+++ update.py
@@ -0,0 +1,7 @@
+'''
+So far, I am on chapter 3 of this workbook. I feel like progress has been a little slow. 
+I am struggling on understanding some parts of the book, and I try to fixate on it until I can understand it. 
+When it comes to the chapter exercises, sometimes I can't understand exactly what the question is asking, such as the "movenexttofront" algorithm
+in chapter 2. Overall, I'm hoping to pick up the pace a little bit, and I think this will happen if I use external resources when I get confused, 
+and try to aim for finishing ~ 2 chapters every 3 weeks. 
+"
