2b0a9d874cc62dd816975ecc6e42826afd580db0
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2023-10-12 18:25:03 -0500

    Create chapyter3.py

---
+++ Exercises/chapyter3.py
@@ -0,0 +1,68 @@
+#1
+#Preorder: E,O,[Blank],A,C,P,M,L,T,E,T,[Blank],E,R,E
+#Inorder: A,[Blank],C,O,P,M,L,E,T,E,T,[Blank],E,R,E
+#Postorder: A,[Blank],C,O,P,M,L,E,T,E,T,[Blank],E,R,E
+#Level order: E,O,T,[Blank],P,E,E,A,C,M,L,T,[Blank],R,E
+
+#2
+# What is the height of a 2-way tree with n-nodes?
+# ??
+
+#3
+#           *
+#         /   \
+#        +     +
+#       / \   / \
+#      A   B C   +
+#               / \
+#              D   E
+
+#4
+# On paper
+
+#5
+'''
+[]
+[P]
+[M L P]
+[S M L P]
+[K A S M L P]
+[A S M L P]
+[S M L P]
+[M L P]
+[L P]
+[E L P]
+[R E L P]
+[T E R E L P]
+[E R E L P]
+[E E R E L P]
+[E R E L P]
+[R E L P]
+[E L P]
+[L P]
+'''
+
+#6
+'''
+[P]
+[P M L]
+[M L]
+[M L S]
+[L S]
+[L S E]
+[S E]
+[S E A]
+[S E A A]
+[E A A]
+[E A A R]
+[A A R]
+[A R]
+[R]
+[R T]
+[R T E]
+[T E]
+[E]
+[E E]
+[E]
+[E]
+'''
