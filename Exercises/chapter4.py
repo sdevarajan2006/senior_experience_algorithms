19db8fff5908d399147c8486b97645a3d56ceba6
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2023-12-04 09:16:13 -0600

    Update chapter4.py - #2 still in progress

--- Exercises/chapter4.py
+++ Exercises/chapter4.py
@@ -15,10 +15,48 @@ def print_tree(tree, indent=0):
     for child_key, child_node in tree["children"].items():
         print_tree(child_node, indent + 4)
 
-def main():
-    binary_tree = generate_binary_tree(3, 200)
-    print_tree(binary_tree)
+#Write a recursive program to compute the external path length of a binary tree
 
-if __name__ == "__main__":
-    main()
+def dfs_recursive(n, time, timedict, ad, depth):
+    timedict[n][0] = time
+    time += 1
+    
+    # Check if the node is a leaf node
+    if len(ad[n]) == 1:
+        # Increment the depth for external nodes
+        depth[0] += time
+    
+    # Check if the node has two children (internal node)
+    if len(ad[n]) == 2 and n != 0:
+        time += 1
+    
+    for i in ad[n]:
+        if timedict[i][0] == 0:
+            time = dfs_recursive(i, time, timedict, ad, depth)
+    
+    timedict[n][1] = time
+    return time
 
+def td(ad):
+    timedict = {}
+    for i in ad.keys():
+        timedict[i] = [0, 0]
+    return timedict
+
+def external_path_length(n, ad):
+    timedict = td(ad)
+    depth = [0]  # To accumulate the external path length
+    time = dfs_recursive(n, 1, timedict, ad, depth)
+    print("External Path Length:", depth[0])
+
+ex = {
+  0: [1, 2],
+  1: [0, 3],
+  2: [0, 4, 5],
+  3: [1],
+  4: [2, 6],
+  5: [2],
+  6: [4]
+}
+
+external_path_length(0, ex)
