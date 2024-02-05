ff195c7f2e8662eae288840f63361a8257ee4de7
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2024-02-05 09:05:26 -0600

    Update chapter5.py

--- Exercises/chapter5.py
+++ Exercises/chapter5.py
@@ -1,1 +1,32 @@
 #starting chapter 5
+
+#1 Suppose it is known that the running time of one algorithm is O(N log N) and that the running time of another algorithm is O(N^3). What does
+#this say about the relative performance of the algorithms?
+
+'''
+As the value of n increases, N^3 will increase at a much faster rate than O(N log N), meaning that O(N log N) is a more efficient algorithm
+'''
+
+#2 Suppose it is known that the running time of one algorithm is always about N log N and that the running time of another algorithm is O(N^3). What does
+# this say about the relative performance of the algorithms?
+'''
+It is difficult to predict which algorithm will be the most efficient, as o(N^3) is a worst case scenario, while N(log N) is an average run time. However,
+I would still assume that N(log N) executes faster, as N^3 is significantly more costly in terms of time
+'''
+
+# 3 Suppose it is known that the running time of one algorithm is N log N and that the running time of another algorithm is O(N^3). What does
+#this say about the relative performance of the algorithms?
+'''
+As the value of n increases, N^3 will increase at a much faster rate than O(N log N), meaning that O(N log N) is a more efficient algorithm
+'''
+
+# 4 Explain the difference between O(1) and O(2)
+'''
+O(2) takes double the amount of time as O(1) in a worst case scenario. However, for both notations, the performance of the algorithm doesn't depend
+on the size of the input 
+'''
+
+# 5 
+
+
+
