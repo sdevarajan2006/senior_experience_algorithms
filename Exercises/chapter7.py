730281a77c0322169b3881c443e6572813556fbe
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2024-03-04 09:13:19 -0600

    Update chapter7.py

--- Exercises/chapter7.py
+++ Exercises/chapter7.py
@@ -1,1 +1,13 @@
+#1 Give a sequence of "compare exchange" operations for sorting four records 
 
+#Bubble sort 
+# 31 25 33 -1
+# 25 31 33 -1
+# 25 31 33 -1
+# 25 31 -1 33
+# 25 31 -1 33
+# 25 -1 31 33
+# -1 25 31 33
+
+#2 Which of the 3 elementary methods (selection sort, insertion sort, or bubble sort) runs fastest for an already sorted file?
+# Insertion sort, because each element is determined to be in the right place and requires no further comparison
