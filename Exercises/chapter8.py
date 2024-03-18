32b7d8345f94dffad677f504dfcac41066ed7bf6
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2024-03-18 09:25:01 -0500

    Update chapter8.py
    finished exercise 1

--- Exercises/chapter8.py
+++ Exercises/chapter8.py
@@ -1,1 +1,69 @@
+import random
+import datetime
+import pytest
 #8- Quicksort 
+#1 Implement a recursive Quicksort with a cutoff to insertion sort for subfiles with less than M elements and empirically determine the value 
+# of M for which it runs fastest on a random file of 1000 elements. 
+
+def insertion_sort(arr):
+  last_key = len(arr) - 1
+  key = 1
+  while key <= last_key:
+    i = key
+    while i > 0 and arr[i - 1] > arr[i]:
+      stor = arr[i]
+      arr[i] = arr[i - 1]
+      arr[i - 1] = stor
+      i -= 1
+    key += 1
+  return arr
+
+def quicksort(l):
+  if len(l) == 0:
+    return []
+  if len(l) <= cutoff:
+    return insertion_sort(l)
+  partitioning_element = l.pop()
+  pels = [partitioning_element]
+  smaller = []
+  larger = []
+  for i in l:
+    if i < partitioning_element:
+      smaller.append(i)
+    elif i > partitioning_element:
+      larger.append(i)
+    else:
+      pels.append(i)
+  sm = quicksort(smaller)
+  lg = quicksort(larger)
+  return sm + pels + lg
+
+#testing it
+
+def test_quicksort():
+  l1 = [23,4,3,12,3,4]
+  assert quicksort(l1) == [3,3,4,4,12,23]
+  l2 = [1,2,3,4,5,6,7,2,4234,64,6,2,234,213,8,9,10]
+  assert quicksort(l2) == [1, 2, 2, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10, 64, 213, 234, 4234]
+  l3 = [1,1]
+  assert quicksort(l3) == [1,1]
+
+# experiment for smallest value of M 
+random_list = [random.randint(1, 100) for _ in range(1000)]
+
+shortest_time = None 
+for cutoff in range(1000):
+  rl = random_list.copy()
+  st = datetime.datetime.now()
+  quicksort(rl)
+  et = datetime.datetime.now()
+  elapsed_time = et - st
+  print(cutoff,'- Execution time:', elapsed_time, 'seconds')
+  if shortest_time is None or elapsed_time < shortest_time:
+    shortest_time = elapsed_time
+    shortest_cutoff = cutoff
+print(shortest_cutoff)
+
+def test_quicksort():
+
+#shortest_cutoff ranges from 10-20 
